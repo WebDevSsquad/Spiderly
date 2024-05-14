70685babc71d00a46bf66176b2617dac858755a6
Youssef Tarek <myoussef9366@gmail.com>
2024-05-14 09:18:02 +0300

    "small fix: add exceptions"

--- JavaServer/src/main/java/com/Spiderly/JavaServer/SearchController.java
+++ JavaServer/src/main/java/com/Spiderly/JavaServer/SearchController.java
@@ -3,8 +3,10 @@ package com.Spiderly.JavaServer;
 import Models.Message;
 import SearchHandler.SearchHandler;
 import org.json.JSONObject;
+import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,7 +15,7 @@ import java.util.Map;
 public class SearchController {
     @CrossOrigin
     @GetMapping("/search")
-    public Map<String, Object> search(@RequestParam String q){
+    public Map<String, Object> search(@RequestParam String q) throws IOException, ParseException {
 //        HashMap<String,Integer> m = new HashMap<>();
 //        JSONObject jsonObject = new JSONObject();
 //        jsonObject.put("name", "John");
